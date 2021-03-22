65f0415f08dd9272799f0d5e6f9ad4a7bd4fafac
nishkarshraj000@gmail.com <nishkarsh.raj@statusneo.com>
2021-03-22 16:52:43 +0530

    Working

--- vidtest.py
+++ vidtest.py
@@ -1,3 +1,3 @@
 from object_Detection_video import object_Detection_video
 vid = "input.mp4"
-print(object_Detection_video().predict(X,["feature_name"]))
+print(object_Detection_video().predict(vid,["feature_name"]))
