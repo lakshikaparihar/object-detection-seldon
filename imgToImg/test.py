3c3edbe95383ed71c5547e7dfdd4d5d675672630
nishkarshraj000@gmail.com <nishkarsh.raj@statusneo.com>
2021-03-23 14:03:00 +0530

    Update

--- imgToImg/test.py
+++ imgToImg/test.py
@@ -1,3 +1,3 @@
 from Model import Model
-img = 'D:\Projects\object-detection-seldon\imgToImg\AIM2.png'
+img = 'AIM2.png'
 print(Model().predict(img,["feature_name"]))
--- imgToJson/test.py
+++ imgToJson/test.py
@@ -1,3 +1,3 @@
 from Model import Model
-img = 'D:\Projects\object-detection-seldon\imgToJson\AIM2.png'
+img = 'AIM2.png'
 print(Model().predict(img,["feature_name"]))
